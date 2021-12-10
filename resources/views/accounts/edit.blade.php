866d8eb8ae1c19323aed76e10a5d4e2152ba6894
LouisGoodland <91783081+LouisGoodland@users.noreply.github.com>
2021-12-10 18:36:44 +0000

    account edit css

--- resources/views/accounts/edit.blade.php
+++ resources/views/accounts/edit.blade.php
@@ -7,14 +7,42 @@
 @section('content')
     <form method="POST" action={{ route('update.account') }} enctype="multipart/form-data">
         @csrf
-        <input type="file"  name="image">
-        <p>First name: <input type="text" name="first_name"
-        value="{{ old('first_name') }}"></p>
-        <p>Last name: <input type="text" name="last_name"
-        value="{{ old('last_name') }}"></p>
-        <p>Date of Birth: <input type="date" name="date_of_birth"
-        value="{{ old('date_of_birth') }}"></p>
+        <div class="row">
+            <div class="col-5">
+                <p class="text-center">Profile picture:</p>
+            </div>
+            <div class="col-7">
+                <input type="file"  name="image">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5">
+                <p class="text-center">First name:</p>
+            </div>
+            <div class="col-7">
+                <input type="text" name="first_name" value="{{ old('first_name') }}">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5">
+                <p class="text-center">Last name:</p>
+            </div>
+            <div class="col-7">
+                <input type="text" name="last_name" value="{{ old('last_name') }}">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5">
+                <p class="text-center">Date of Birth:</p>
+            </div>
+            <div class="col-7">
+                <input type="date" name="date_of_birth" value="{{ old('date_of_birth') }}">
+            </div>
+        </div>
+        <br>
+        <br>
         
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" class="position-relative top-50 start-50 translate-middle">
+
     </form>
 @endsection
